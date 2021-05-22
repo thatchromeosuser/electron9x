a4e15dde5c0413fb256abaad9a7501845bfa9a52
i farted <83254358+thatchromeosuser@users.noreply.github.com>
2021-05-21 21:11:18 -0500

    Add files via upload

---
+++ sys/desktop.js
@@ -0,0 +1,384 @@
+system42("desktop", function(le) {
+  "use strict";
+  le._temp.defaultDesk = [
+    {
+      name: "WINDOWS 93.lnk42",
+      exe: "/",
+      row: 0,
+      col: 0,
+    },
+    {
+      name: "System (C:).lnk42",
+      exe: "/c/",
+      row: 1,
+      col: 0,
+    },
+    {
+      name: "Storage (A:).lnk42",
+      exe: "/a/",
+      row: 1,
+      col: 1,
+    },
+    {
+      name: "Terminal.lnk42",
+      exe: "hello",
+      icon: "apps/terminal.png",
+      row: 2,
+      col: 0,
+    },
+
+    {
+      name: "Cat Explorer.lnk42",
+      exe: "catex",
+      row: 2,
+      col: 1,
+    },
+    {
+      name: "Zkype.lnk42",
+      exe: "zkype",
+      row: 2,
+      col: 2,
+    },
+    {
+      name: "Clavardage.lnk42",
+      exe: "trollbox",
+      icon: "apps/chat.gif",
+      row: 2,
+      col: 3,
+    },
+    {
+      name: "MESS.ENGER®.lnk42",
+      exe: "messenger",
+      row: 2,
+      col: 4,
+    },
+    {
+      name: "Piskel.lnk42",
+      exe: "piskel",
+      row: 3,
+      col: 0,
+    },
+    {
+      name: "CodeMirror.lnk42",
+      exe: "code",
+      row: 3,
+      col: 1,
+    },
+    {
+      name: "HexEd.lnk42",
+      exe: "hexed",
+      row: 3,
+      col: 2,
+    },
+    {
+      name: "3d.lnk42",
+      exe: "3d",
+      row: 3,
+      col: 3,
+    },
+    {
+      name: "Calc.lnk42",
+      exe: "calc",
+      row: 3,
+      col: 4,
+    },
+    {
+      name: "Bananamp.lnk42",
+      exe: "bananamp",
+      row: 3,
+      col: 5,
+    },
+	/*
+    {
+       name: "NECRONOMI COIN.lnk42",
+       exe: "necronomicoin",
+       icon: "/c/files/images/icons/necronomicoin.png",
+       row: 3,
+       col: 6,
+    },*/
+
+
+    /*
+    {
+      name: "PENG!.lnk42",
+      exe: "peng",
+      row: 3,
+      col: 6,
+    },*/
+    {
+      name: "Poney Jockey.lnk42",
+      exe: "pony",
+      row: 4,
+      col: 0,
+    },
+    {
+      name: "Byte Beat.lnk42",
+      exe: "bytebeat",
+      row: 4,
+      col: 1,
+    },
+    {
+      name: "Puke Data.lnk42",
+      exe: "pd",
+      row: 4,
+      col: 2,
+    },
+    {
+      name: "Speech.lnk42",
+      exe: "speech",
+      row: 4,
+      col: 3,
+    },
+    {
+      name: "LSDJ.lnk42",
+      exe: "/c/files/roms/dmg/music/lsdj3_1_9_demo.gb",
+      icon: "apps/lsdj.png",
+      row: 4,
+      col: 4,
+    },
+    {
+      name: "Nanoloop.lnk42",
+      exe: "/c/files/roms/dmg/music/nanoloop171d.gb",
+      icon: "apps/nanoloop.png",
+      row: 4,
+      col: 5,
+    },
+    {
+      name: "Glitch Grrrlz.lnk42",
+      exe: "/c/files/roms/dmg/fail/Glitch_Grrrlz-Vol01.gbc",
+      icon: "apps/glitchgirlz.png",
+      row: 4,
+      col: 6,
+    },
+    {
+      name: "FX.lnk42",
+      exe: "fx",
+      row: 4,
+      col: 7,
+    },
+    {
+      name: "Robby.lnk42",
+      exe: "robby",
+      row: 5,
+      col: 0,
+    },
+    {
+      name: "Take this.lnk42",
+      exe: "takethis",
+      row: 5,
+      col: 1,
+    },
+    {
+      name: "Pokéglitch.lnk42",
+      exe: "/c/files/roms/dmg/fail/Pokeglitch.gb",
+      title: "Pokéğ̭̤ͬ̋ͭͬ̏ͪl̥̠ͦiṫ͚̠͇̠̮̯̍ͬ͗̂c̙ͭ͛̄h̫̦ͯ͆ͯ̀̓̂ͫ",
+      icon: "apps/helix.png",
+      row: 5,
+      col: 2,
+    },
+    {
+      name: "What If.lnk42",
+      exe: "whatif",
+      row: 5,
+      col: 3,
+    },
+    {
+      name: "Virtual PC.lnk42",
+      exe: "virtualpc",
+      icon: "apps/inception.png",
+      row: 5,
+      col: 4,
+    },
+    {
+      name: "HAMPSTER.lnk42",
+      exe: "hampster",
+      row: 5,
+      col: 5,
+    },
+    {
+      name: "Half-Life 3.lnk42",
+      exe: "hl3",
+      row: 5,
+      col: 6,
+    },
+    {
+      name: "ANTHOLOGY.AVI.lnk42",
+      exe: "anthology",
+      icon: "/c/sys/skins/w93/trophy.gif",
+      row: 5,
+      col: 7,
+    },
+
+    /*
+    // PROMO MODE OFF
+    {
+      name: "POTATO.AVI.lnk42",
+      exe: "potato",
+      icon: "/c/files/images/icons/potato.png",
+      row: 5,
+      col: 7,
+    },
+    */
+
+    /*
+    // PROMO MODE OFF
+    {
+      name: "SUPER PLAYER.lnk42",
+      exe: "superplayer",
+      icon: "/c/files/images/icons/derp.png",
+      row: 5,
+      col: 8,
+    },
+    */
+    // {
+    //   name: "lenna.png.lnk42",
+    //   exe: "lenna",
+    //   row: 6,
+    //   col: 0,
+    // },
+    {
+      name: "Star Wars.avi.lnk42",
+      exe: "starwars",
+      icon: "apps/yoda.gif",
+      row: 6,
+      col: 0,
+    },
+    {
+      name: "Corglitch.lnk42",
+      exe: "glitch",
+      icon: "apps/corgi.gif",
+      row: 6,
+      col: 1,
+    },
+    {
+      name: "Doctor Marburg.lnk42",
+      exe: "doctor",
+      row: 6,
+      col: 2,
+    },
+    {
+      name: "DEREG32.EXE.lnk42",
+      exe: "marburg",
+      icon: "apps/sick.png",
+      row: 6,
+      col: 3,
+    },
+    {
+      name: "SkiFree.lnk42",
+      exe: "skifree",
+      row: 6,
+      col: 4,
+    },
+    {
+      name: "Progress Quest.lnk42",
+      exe: "progressquest",
+      icon: "apps/progressquest.gif",
+      row: 6,
+      col: 5,
+    },
+    {
+      name: "KOF 93.lnk42",
+      exe: "kof93",
+      icon: "/c/programs/kof93/gief.gif",     
+      row: 6,
+      col: 6,
+    },    
+
+    {
+      name: "Virtual Girl.lnk42",
+      exe: "lisa",
+      icon: "apps/annaKournikova.gif",
+      row: 7,
+      col: 0,
+    },
+    {
+      name: "Hydra.exe.lnk42",
+      exe: "hydra",
+      icon: "apps/hydra.png",
+      row: 7,
+      col: 1,
+    },
+    {
+      name: "Acid Box 93.lnk42",
+      exe: "acidbox",
+      row: 7,
+      col: 2,
+    },
+    {
+      name: "Arena 93.lnk42",
+      exe: "arena93",
+      row: 7,
+      col: 3,
+    },
+    {
+      name: "Game Of Life.lnk42",
+      exe: "gameoflife",
+      row: 7,
+      col: 4,
+    },
+    {
+      name: "Solitude.lnk42",
+      exe: "solitude",
+      row: 7,
+      col: 5,
+    },
+    {
+      name: "Brian Sweeper.lnk42",
+      exe: "mines",
+      row: 7,
+      col: 6,
+    },
+    {
+      name: "Gafa3D.lnk42",
+      exe: "castlegafa",
+      row: 7,
+      col: 7,
+    },
+    {
+      name: "Defrag.lnk42",
+      exe: "defrag",
+      row: 7,
+      col: 8,
+    },
+    {
+      name: "Totally not a virus.Trust me...im a dolphin.lnk42",
+      exe: "gravity",
+      icon: "apps/dolphin.png",
+      row: 8,
+      col: 0,
+    },
+    {
+      name: "OLDEST GIF OF THE INTERWEBZ.gif.lnk42",
+      exe: "/c/files/images/gif/wind.gif",
+      row: 8,
+      col: 1,
+    },
+    {
+      name: "Contact us.lnk42",
+      exe: "contact",
+      icon: "mail.png",
+      row: 8,
+      col: 2,
+    },
+    {
+      name: "Credits.txt.lnk42",
+      exe:
+        'iframe /c/credits.html --width=450 --height="300" --bodyClass="skin_light skin_inset"',
+      row: 8,
+      col: 3,
+    },
+    {
+      name: "MANIFESTO.lnk42",
+      exe: "manifesto",
+      row: 8,
+      col: 4,
+    },
+    {
+      name: "TRASH.lnk42",
+      exe: "/a/trash/",
+      icon: "trash.png",
+      row: 100,
+      col: 0,
+    },
+  ];
+});
